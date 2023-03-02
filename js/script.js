373bf3d2acabcda11f392d8c7efad046662ed215
Nguyễn Minh Huy <119658952+mhuy14@users.noreply.github.com>
2023-03-02 10:41:42 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function handleInput(evt) {
+    const value = evt.target.value
+    const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+    
+    if (emailRegex.test(value.trim())) {
+        evt.target.classList.add('valid')
+        evt.target.classList.remove('invalid')
+    } else {
+        evt.target.classList.add('invalid')
+        evt.target.classList.remove('valid')
+    }
+
+    if (!value) {
+        evt.target.classList.remove('invalid')
+    }
+}
+$(function() {
+    $('.footer-links-holder h3').click(function () {
+      $(this).parent().toggleClass('active');
+    });
+  });
